b63dd4d2f97a5e927013300e4c77c35e2ba5a86c
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2022-05-26 19:51:46 +0200

    updates!

--- popup.js
+++ popup.js
@@ -5,44 +5,45 @@ document.getElementById("add-website-button").addEventListener("click", AddWebsi
 
 function AddWebsite() {
     let the_website_url = document.getElementById("website-url").value;
-    Blocked_websites.push(the_website_url);
+    if(!Blocked_websites.includes(the_website_url)){
+
+        Blocked_websites.push(the_website_url);
+
+        let li = document.createElement('li');
+        li.style.width="fit-content";
+        li.style.display="flex";
+        li.style.margin="5px";
+        li.style.border="solid";
+        li.style.borderRadius="20px";
+        li.style.backgroundColor="rgb(247, 174, 64)";
+        li.style.padding="5px";
+
+        
+
+        let label = document.createElement('label');
+        label.innerHTML = "Website Url:"
+        label.style.marginRight="10px";
+        li.appendChild(label);
 
 
-    let li = document.createElement('li');
-    li.style.width="fit-content";
-    li.style.display="flex";
-    li.style.margin="5px";
-    li.style.border="solid";
-    li.style.borderRadius="20px";
-    li.style.backgroundColor="rgb(247, 174, 64)";
-    li.style.padding="5px";
 
-    
-
-    let label = document.createElement('label');
-    label.innerHTML = "Website Url:"
-    label.style.marginRight="10px";
-    li.appendChild(label);
-
-
-
-    let h7 = document.createElement('h7');
-    li.innerHTML = the_website_url;
-    h7.style.marginRight="10px";
-    li.appendChild(h7);
-    
-    
-    
-    let button = document.createElement('button');
-    button.innerHTML = "Remove"
-    button.style.marginRight="2px";
-    button.style.borderRadius="20px";
-    button.style.backgroundColor="rgb(244, 70, 35)";
-    button.style.cursor="pointer";
-    li.appendChild(button);
-    
-    document.getElementById("container").appendChild(li);
-
+        let h7 = document.createElement('h7');
+        li.innerHTML = the_website_url;
+        h7.style.marginRight="10px";
+        li.appendChild(h7);
+        
+        
+        
+        let button = document.createElement('button');
+        button.innerHTML = "Remove"
+        button.style.marginRight="2px";
+        button.style.borderRadius="20px";
+        button.style.backgroundColor="rgb(244, 70, 35)";
+        button.style.cursor="pointer";
+        li.appendChild(button);
+        
+        document.getElementById("container").appendChild(li);
+    }
 }
 
 
